4251b9b974b5b9510617ad3caca72c54e8956968
jubayer@theclinician.com <Jubayer Mohammed>
2023-09-02 21:09:22 +0600

    init project api part

---
+++ dynamic-dashboard-backend/src/test/java/com/juba13/dynamicdashboardbackend/DynamicDashboardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juba13.dynamicdashboardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DynamicDashboardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
