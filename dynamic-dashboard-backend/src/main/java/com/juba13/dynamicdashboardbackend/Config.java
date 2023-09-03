1cc3af547f6c799373d32b1921749fd80e75f75c
Mohammed Jubayer <mohammed.jubayer@gmail.com>
2023-09-03 10:57:42 +0600

    Added config class

---
+++ dynamic-dashboard-backend/src/main/java/com/juba13/dynamicdashboardbackend/Config.java
@@ -0,0 +1,38 @@
+package com.juba13.dynamicdashboardbackend;
+/**
+ * Configuration class for the Dashboard application.
+ */
+public class AppConfig {
+    // Base context path for the application
+    public static final String CONTEXT = "/dynamic-dashboard/";
+
+    // Default server port
+    public static final String PORT = "8282";
+
+    // Paths that are not filtered (excluded from filtering)
+    public static String[] NON_FILTERING_PATH_PATTERNS = new String[]{
+            "/swagger-resources/configuration/ui",
+            "/configuration/ui",
+            "/swagger-resources",
+            "/swagger-resources/configuration/security",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**"
+    };
+
+    // Paths that are filtered
+    public static String[] FILTERING_PATH_PATTERNS = new String[]{"/**"};
+
+    // Private key for JWT authentication
+    public static final String APPLICATION_JWT_PRIVATE_KEY = "sjsjfdsjfidsjfiamsadsadisadsadosadsakdsad";
+
+    /**
+     * Inner class containing keys for session attributes.
+     */
+    static public class KEY {
+        public static String USER_ID = "USER_ID";     // User ID key
+        public static String ORG_ID = "ORG_ID";   // Organization ID key
+        public static String SESSION_ID = "SESSION_ID"; // Session ID key
+        public static String TOKEN = "TOKEN";         // Token key
+    }
+}
